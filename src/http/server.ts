290ce6f7290a60e6c698c74d8e795b79dceaef79
Felippe Santana <felippefsantana02@gmail.com>
2024-02-08 11:56:25 -0300

    get poll and vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,20 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 const port = 3333;
 
+app.register(cookie, {
+  secret: "FDfskjf328A!3:fl;?",
+  hook: "onRequest"
+});
+
 app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port }).then(() => {
   console.log(`HTTP server listening on ${port}`);
